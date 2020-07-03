483706db44e79e35c02bc17fdad0905567ea19a4
Tiago Soares <fullcodebr@gmail.com>
2020-07-03 00:16:25 -0300

    jwt helper setup

---
+++ src/helpers/jwt.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const tokenPrivateKey = process.env.JWT_PRIVATE_TOKEN_KEY;
+const refreshTokenPrivateKey = process.env.JWT_REFRESH_PRIVATE_TOKEN_KEY;
+const options = {
+    expiresIn: '30 minutes'
+};
+
+const refreshOptions = {
+    expiresIn: '30 days'
+};
+
+const generateJwt = payload => {
+
+    return jwt.sign(payload, tokenPrivateKey, options);
+}
+
+const generateRefreshJwt = payload => {
+
+    return jwt.sign(payload, refreshTokenPrivateKey, refreshOptions);
+}
+
+const verifyJwt = token =>{
+
+    return jwt.verify(token, tokenPrivateKey)
+}
+
+const verifyRefreshJwt = token =>{
+
+    return jwt.verify(token, refreshTokenPrivateKey)
+}
+
+
+module.exports = {
+    generateJwt,
+    generateRefreshJwt,
+    verifyJwt,
+    verifyRefreshJwt
+}
