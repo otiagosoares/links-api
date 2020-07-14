1abe142f5c9f97ba39d3b2b155ff63b6eb69d9c6
Tiago Soares <fullcoderb@gmail.com>
2020-07-13 23:47:13 -0300

    CRUD links

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express =  require('express');
 const app = express();
 const db = require('./models');
 const authController = require('./conttrollers/auth');
+const linkController = require('./conttrollers/link');
 const response = require('./middlewares/response');
 
 app.use(response);
@@ -9,9 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/auth', authController);
+app.use('/links', linkController);
 
 app.get('/', (req, res) => {
-    return res.json('Api is runnibg...');
+    return res.json('Api is running...');
 })
 
 
